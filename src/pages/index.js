1f9135dfb7e822ae05d1e583fd21266ddfdb9786
Karimouloud <karimachoui@gmx.fr>
2021-11-09 16:53:32 +0100

    ajout nav

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Container } from '../components'
 
-export const IndexPage = () => {
+const IndexPage = () => {
   return(
     <Container>
       <h1>Hello world!</h1>
