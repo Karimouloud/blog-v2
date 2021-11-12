7d77a21f78cbaa74480b1f42094cb708dc1eac6c
Karimouloud <karimachoui@gmx.fr>
2021-11-12 17:21:52 +0100

    ajout content card

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,17 @@
 import React from "react"
-import { Container, FeatureImage } from '../components'
+import { Container, Content, ContentCard, FeatureImage } from '../components'
 
 const IndexPage = () => {
   return(
     <Container>
       <FeatureImage />
+      <Content>
+        <ContentCard 
+          date="Novembre 12, 2021" 
+          title="blog sport" 
+          excerpt="muscu et cardio"
+          slug="/test" />
+      </Content>
     </Container>
     )
 }
